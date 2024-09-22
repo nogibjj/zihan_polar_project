08e92743991193f4f236844ea79f63f94afc9591
Zihan Xiao <98075925+zihanxiao23@users.noreply.github.com>
2024-09-22 18:54:46 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,13 +1,1 @@
-from main import get_statistics, build_barplot
-
-def test_statistics():
-    result1 = get_statistics()
-    assert result1 is None
-
-def test_plot():
-    result2 = build_barplot()
-    assert result2 is None
-
-if __name__=="__main__":
-    test_statistics()
-    test_plot()
+# no need to test this time
