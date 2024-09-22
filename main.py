8fc291eeec2e162511083c211cb5fd2d47032b01
Zihan Xiao <98075925+zihanxiao23@users.noreply.github.com>
2024-09-22 19:14:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt  # imports the package and calls it 'plt'
 from ydata_profiling import ProfileReport  # import it for EDA
 
 # get a rough view of what the data looks like - the list of all the world's billionaires from 2021
-df = pl.read_csv("Billionaire_2021.csv", dtypes={"Age": pl.Float64})
+df = pl.read_csv("Billionaire_2021.csv", null_values=["N/A"])
 print(df.head())
 
 # use dataprofiling to do EDA for this dataset
